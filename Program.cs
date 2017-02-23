5e15dad60d6891f06f71925f818fee0acc0a8465
Luis Alfredo Suárez Litardo <junior2pp@hotmail.com>
2017-02-23 18:58:33 -0500

    subido

--- Program.cs
+++ Program.cs
@@ -2,206 +2,109 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
-using System.Threading;
-using System.Timers;
-namespace nim
+namespace Matriz7
 {
-    class Program
+    class Matriz7
     {
+        private int[,] mat;
 
-        public void inicio()
+        public void Cargar()
         {
-            Console.SetCursorPosition(45, 1);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("BIENVENIDO AL JUEGO NIM SENCILLO");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\n[INSTRUCCIONES]");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(" \n\n[Jugadores] =  2 \n[Objetivo] = Intentar quitar los últimos palitos de la Consola \n[Jugando]\n * Se dispone de 16 palitos en la Consola \n * Al inicio el jugador quita 1 o 2 palitos de la Consola. \n * Cuando el jugador quita 1 o 2 palitos termina su turno \n * Gana el jugador que saca los ultimos palitos \n * Tiene un tiempo Disponible de 7 segundos por Turno");
-            Console.SetCursorPosition(1, 25);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("by Luis Alfredo Suárez Litardo \n Telemática 3er Semestre \n Materia: Árboles y Teória de Juegos \n Profesor: Ing.Marcillo Vera");
-        }
-        public void mostrar( Stack pila1)
-        {
-            Console.SetCursorPosition(45, 4);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("PALILLOS RESTANTE");
-            Console.SetCursorPosition(40, 6);
-            Console.ForegroundColor = ConsoleColor.Green;
-            foreach (var item in pila1)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-        }
-        public void empezar()
-        {
-            
-            Console.ForegroundColor = ConsoleColor.Red;
-            Stack pila = new Stack();
-            int n = 16;
-            for (int i = 0; i < n; i++)
-            {
-                pila.Push("|");
-            }
+            Console.Write("Cuantas fila tiene la matriz:");
+            string linea;
+            linea = Console.ReadLine();
+            int filas = int.Parse(linea);
+            Console.Write("Cuantas columnas tiene la matriz:");
+            linea = Console.ReadLine();
 
-            Program gato = new Program();
-            gato.inicio();
-            Console.SetCursorPosition(45, 15);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("PRESIONA UNA TECLA PARA JUGAR");
+            int columnas = int.Parse(linea);
+            mat = new int[filas, columnas];
+            Random r = new Random();
+            for (int f = 0; f < mat.GetLength(0); f++)
+            {
+                for (int c = 0; c < mat.GetLength(1); c++)
+                {
+                   
+                    mat[f, c] = r.Next(1,10);
+                }
+            }
+        }
+
+        public void Intercambiar()
+        {
+            ////ESTE SIRVE PARA INTERCAMBIAR LAS COLUMNAS
+            Console.SetCursorPosition(15, 10);
+            Console.Write("Que Columna desea intercambiar # 1 : ");
+            int colu1 = int.Parse(Console.ReadLine());
+            Console.SetCursorPosition(15, 11);
+            Console.Write("Que Columna desea intercambiar # 2 : ");
+            int colu2 = int.Parse(Console.ReadLine());
+            for (int c = 0; c < mat.GetLength(0); c++)
+            {
+                int aux = mat[colu1, c];
+                mat[colu1, c] = mat[colu2, c];
+                mat[colu2, c] = aux;
+            }
+            for (int f = 0; f < mat.GetLength(0); f++)
+            {
+                for (int c = 0; c < mat.GetLength(1); c++)
+                {
+                    Console.SetCursorPosition((5 * f) + 25, (2 * c) + 5);
+                    Console.Write(mat[f, c] + " ");
+                }
+            }
+        }
+
+
+        public void intercambiarfilas()
+        {
+            Console.SetCursorPosition(15, 14);
+            Console.Write("Que fila desea intercambiar # 1 : ");
+            int colu1 = int.Parse(Console.ReadLine());
+            Console.SetCursorPosition(15, 15);
+            Console.Write("Que fila desea intercambiar # 2 : ");
+            int colu2 = int.Parse(Console.ReadLine());
+            for (int c = 0; c < mat.GetLength(1); c++)
+            {
+                int aux = mat[c, colu1];
+                mat[c, colu1] = mat[c, colu2];
+                mat[c, colu2] = aux;
+            }
+            for (int f = 0; f < mat.GetLength(1); f++)
+            {
+                for (int c = 0; c < mat.GetLength(1); c++)
+                {
+                    Console.SetCursorPosition((5 * f) + 45, (2 * c) + 5);
+                    Console.Write(mat[f, c]);
+                }
+            }
+        }
+        public void Imprimir()
+        {
+            for (int f = 0; f < mat.GetLength(0); f++)
+            {
+                for (int c = 0; c < mat.GetLength(1); c++)
+                {
+                    Console.SetCursorPosition((5*f)+10,(2*c)+5);
+                    Console.Write(mat[f, c] + " ");
+                }
+            }
             Console.ReadKey();
-            Console.SetCursorPosition(45, 18);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Nombre del Jugador 1: ");
-            string juga1 = Console.ReadLine();
-            Console.SetCursorPosition(45, 20);
-            Console.Write("Nombre del Jugador 2: ");
-            string juga2 = Console.ReadLine();
-            string ganador = "";
-            Console.WriteLine();
-
-            // Console.Write("TIEMPO: " + total.ToString());
-            Console.Clear();
-            do
-            {
-                Console.SetCursorPosition(45, 5);
-                gato.mostrar(pila);
-                bool dato = true;
-                string jugador1 = "";
-                string jugador2 = "";
-
-                int cont1 = 0;
-                ganador = juga2;
-                while (dato)
-                {
-
-                    Console.SetCursorPosition(40, 8);
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write("{0} Puedes sacar 1 o 2 Palillos: ", juga1);
-                    jugador1 = Console.ReadLine();
-                    if (jugador1 == "1" || jugador1 == "2")
-                    {
-                        dato = false;
-                    }
-                    else
-                    {
-                        cont1++;
-                        Console.SetCursorPosition(40, 9);
-                        Console.WriteLine("Por favor Ingrese 1 o 2");
-                    }
-                    if (cont1 > 3)
-                    {
-                        Console.SetCursorPosition(40, 9);
-                        Console.WriteLine("Por favor No seas Estúpido/a mete  1 o 2");
-                    }
-
-
-                }
-
-                if (pila.Count <= 2)
-                {
-                    if (jugador1 == "2")
-                    {
-                        ganador = juga1;
-                        break;
-                    }
-                    if (pila.Count == 1 && jugador1 == "1")
-                    {
-                        ganador = juga1;
-                        break;
-                    }
-                }
-                int j = int.Parse(jugador1);
-                for (int i = 0; i < j; i++)
-                {
-                    pila.Pop();
-                }
-                Console.Clear();
-                gato.mostrar(pila);
-                bool dato2 = true;
-                int conta = 0;
-                while (dato2)
-                {
-                    Console.SetCursorPosition(40, 8);
-                    Console.Write("{0} Puedes sacar 1 o 2 Palillos: ", juga2);
-                    jugador2 = Console.ReadLine();
-                    if (jugador2 == "1" || jugador2 == "2")
-                    {
-                        dato2 = false;
-                    }
-                    else
-                    {
-                        conta++;
-                        Console.SetCursorPosition(40, 11);
-                        Console.WriteLine("Por favor Ingrese 1 o 2 ");
-                    }
-                    if (conta > 3)
-                    {
-                        Console.SetCursorPosition(40, 11);
-                        Console.WriteLine("Por favor No seas Estúpido Mete  1 o 2 ");
-                    }
-
-                }
-
-                if (pila.Count <= 2)
-                {
-                    if (jugador2 == "2")
-                    {
-                        ganador = juga2;
-                        break;
-                    }
-                    if (pila.Count == 1 && jugador2 == "1")
-                    {
-                        ganador = juga2;
-                        break;
-                    }
-                }
-                int z = int.Parse(jugador2);
-                for (int i = 0; i < z; i++)
-                {
-                    pila.Pop();
-                }
-                Console.Clear();
-
-            } while (pila.Count != 0);
-            Console.Clear();
-            Console.SetCursorPosition(50, 5);
-            Console.Write("SIN PALILLOS");
-            Console.SetCursorPosition(45, 15);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("EL GANADOR ES " + ganador.ToUpper());
         }
+
         static void Main(string[] args)
         {
-
-            Program luis = new Program();
-            luis.empezar();
-            bool estado = true;
-            while (estado)
-            {
-                Console.SetCursorPosition(30, 18);
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("Desea Volver a Jugar SI/NO ===>>> ");
-                string respuesta = Console.ReadLine();
-                if (respuesta.ToUpper() == "SI" )
-                {
-                    Console.Clear();
-                    luis.empezar();
-                }
-                if (respuesta.ToUpper() == "NO")
-                {
-                    estado = false;
-                }
-            }
-            Console.SetCursorPosition(30, 20);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("ESTE JUEGO SE CERRARA EN 5 SEGUNDOS");
-            Thread.Sleep(5000);
-            
+            Matriz7 ma = new Matriz7();
+            ma.Cargar();
+            ma.Imprimir();
+            ma.Intercambiar();
+            ma.intercambiarfilas();
+            Console.ReadKey();
+            //ma.Imprimir();
         }
     }
 }
+
+
+
+
